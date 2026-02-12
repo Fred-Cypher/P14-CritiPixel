dd72fcb3013207a9bc0c0d62d7e0f3b3963823d9
Fred <frederique.perrier@gmail.com>
2026-02-12 13:49:30 +0100

    Add unit test for calculteAverageRating (WIP)

---
+++ tests/Unit/RatingHandlerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Tests\Unit;
+
+use App\Model\Entity\Review;
+use App\Model\Entity\VideoGame;
+use App\Rating\RatingHandler;
+use PHPUnit\Framework\TestCase;
+
+class RatingHandlerTest extends TestCase
+{
+    private RatingHandler $handler;
+    protected function setUp(): void
+    {
+        $this->handler = new RatingHandler();
+    }
+
+    public function testCalculateAverageRating(): void
+    {
+        $videoGame = new VideoGame();
+
+        $review1 = (new Review())->setRating(2)->setVideoGame($videoGame);
+        $review2 = (new Review())->setRating(4)->setVideoGame($videoGame);
+        $review3 = (new Review())->setRating(0)->setVideoGame($videoGame);
+
+        $videoGame->getReviews()->add($review1);
+        $videoGame->getReviews()->add($review2);
+        $videoGame->getReviews()->add($review3);
+
+        $this->handler->calculateAverage($videoGame);
+
+        $this->assertEquals(2, $videoGame->getAverageRating());
+    }
+}
